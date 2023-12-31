201e25e9339f85dd2489a237a781935836300f45
Master of None <131793324+yc-pro-123@users.noreply.github.com>
2023-12-31 10:31:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,82 @@
+import re, time, os
+from flask import Flask, request,render_template
+import requests
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/',methods=['GET','POST'])
+def index():
+   rollnum,passw=request.args.get("rno"),request.args.get("pass")
+   print("hii")
+   if request.method == "GET":
+      print(rollnum)
+      print(passw)
+      url = os.getenv("db_url") 
+      #payload = json.dumps( {"rollNo": "xyz","dob": "26012003","data":{ "DEC23":"HSJSJWJWJ","WJWJ":"WHWH"}} )
+      heyy={"q":json.dumps({"rollNo": f"{rollnum}"})}
+      print(heyy)
+      
+      headers = {
+    'content-type': "application/json",
+    'x-apikey': os.getenv("x-apikey") ,
+    'cache-control': "no-cache"
+    }
+      
+      response = requests.get(url,params=heyy, headers=headers)
+      temp=response.json()[0] 
+      #print(type(temp.get("dob")),type(passw))
+      if (str(temp.get("dob"))== passw):
+         print("haleluya")
+         tempor=temp.get("data")
+         key=list(tempor.keys())
+         return render_template("essay.html",key=key,temp=tempor)
+      else:
+         return render_template("invalid.html")
+
+def main(abc):
+   url = os.getenv("db_url")
+   #payload = json.dumps( {"rollNo": abc ,"dob": "26012003","data":{ "DEC23":"HSJSJWJWJ","WJWJ":"WHWH"}} )
+   headers = {
+    'content-type': "application/json",
+    'x-apikey': os.getenv("x-apikey"),
+    'cache-control': "no-cache"
+    }
+   heyy={"q":json.dumps({"rollNo": f"{abc}"})}
+   print(heyy)
+   #time.sleep(4)
+   response = requests.get(url,params=heyy,headers=headers)
+   print(response)
+   temp=response.json()[0].get("data")
+   #print(temp)
+   key=temp.keys()
+   p=""
+   for i in key:
+      p=f"<tr>{i}</tr><tr>{temp[i]}</tr>"
+      #print(i,"\t", temp[i])
+
+   #pastedf rom APP
+   p=f"""<html><h1>HODOHD</h1><table>
+   <tr><th>Company</th>
+   <th>Contact</th>
+   <th>Country</th>
+  </tr>
+  <tr>
+    <td>{rollnum}</td>
+    <td>{passw}</td>
+    <td>Germany</td>
+  </tr>"""
+   print(key)
+   
+   for i in key:
+      p+=f"<tr><td>{key.index(i)+1}</td><td>{i}</td><td>{temp[i]}</td></tr>"
+   p+=f"""</table></html>"""
+   return p
+
+
+
+
+#if __name__ == '__main__':
+#   app.run(debug = True)
+#   main("xyz")
